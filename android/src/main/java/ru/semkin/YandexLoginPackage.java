23a5bfaeddf9d26ed67e9b1386aaf8d6ee305a9a
Ivan Semkin <ivan@semkin.ru>
2020-06-18 11:43:54 +0300

    Add module skeleton

---
+++ android/src/main/java/ru/semkin/YandexLoginPackage.java
@@ -0,0 +1,23 @@
+package ru.semkin;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.bridge.JavaScriptModule;
+
+public class YandexLoginPackage implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        return Arrays.<NativeModule>asList(new YandexLoginModule(reactContext));
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
